cf5f7e5d83408b44b82266253d5a317d711fec16
Harrison Powers <fijimunkii@gmail.com>
2013-12-11 21:24:53 -0500

    removed rails project

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Booki::Application.config.secret_token = '5cfd672e0e1e384734c32912e0945b1122aa47df0a4abb01c59df1c05bbd13a1e18e5ce88da23420bf5b470b65b071509077d1dd4993468e070ac7854db23349'
